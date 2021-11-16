9e819cd65b5ed0bd6eaaa2540ee3b6151ddf3899
mifarmer <89886056+rackettown@users.noreply.github.com>
2021-11-16 00:35:21 +0000

    Grid applied with separate colors for each box
    link nav displayed horizontally
    
    Text stylization applied for each box
    
    3 buttons to change color of article applied through javascript

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function footer(){
   var x = document.lastModified;
   document.getElementById("date").innerHTML = x;
 }
+
+function yellowButton() {
+   document.getElementById("article").style.backgroundColor = "LightGoldenRodYellow";
+}
+
+function pinkButton() {
+  document.getElementById("article").style.backgroundColor = "lightpink";
+}
+
+function redButton() {
+  document.getElementById("article").style.backgroundColor = "lightcoral";
+}
